1e611808c7eca02c0bf69591f9202c7fa7569541
Miasoiedov Denys <grocerytrade@gmail.com>
2022-05-21 19:02:42 +0300

    added files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+    const colorCard = document.querySelectorAll('li:nth-child(odd)');
+    for (let li of colorCard) {
+        li.style.backgroundColor = 'red';
+    }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+
+    let elementLi = document.querySelector('li');
+    while (elementLi) {
+        if (elementLi.matches('.likedItem')) {
+            elementLi.style.backgroundColor = 'blue';
+            break
+        } else {
+            elementLi = elementLi.nextElementSibling;
+        }
+    }
+};
